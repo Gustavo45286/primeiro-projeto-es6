8f1e2244ed2461dc296641013fcbddd11b32cef7
Gustavo <Gustavo45286@hotmail.com.br>
2020-03-12 09:03:17 -0300

    escopos let e var

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
-console.log("Hello world");
+//declarar variaveis
 
-class List{
-    constructor(){
-        this.data = [];
+//var nomeVaiavel;
+
+const nomeVaiavel = 'joao';
+
+const usuario = { nome: 'jesus', idade: 26};
+
+usuario.nome = 'maria';
+
+const meuVetor = [1,2,3,4];
+meuVetor[0] = 90;
+meuVetor[2] = 93;
+
+console.log(usuario);
+console.log(meuVetor);
+
+//variavel do tipo let
+
+let valor = 3;
+let nome ='claudia';
+
+console.log(valor,nome);
+
+
+//escopo A
+
+var exibirMensagem2 = function (){
+    //escopo b
+    if(true){
+        //escopo f
+        let escopoFuncao = "teste";
+        let escopoBloco = "teste 2";
+        console.log(escopoBloco);
+
     }
 
-    add(value){
-        this.data.push(value);
-        this.print;
-        console.log(this.data);
-    }
-
-    Pop(value){
-        this.data.pop();
-        this.print;
-        console.log(this.data);
-    }
-
-    print(){
-        console.log(this.data);
-    }
+    console.log(escopoBloco);
+    console.log(escopoFuncao);
 }
 
-
-class TodoList extends List{
-   
-
-}
-
-const minhaLista = new TodoList;
-
-document.getElementById("novoTodo").onclick = function(){
-    minhaLista.add("abc");
-};
-
-document.getElementById("popTodo").onclick = function(){
-    minhaLista.Pop();
-};
+exibirMensagem2();
+//fsdgtrs
